dc0b709d6a4cf8560f907d6ab3abf799354ca8f4
VincentBai-dotcom <butcheryyy@gmail.com>
2023-12-13 18:03:03 -0500

    add findByID service

---
+++ src/database/word-entry/dto/find-by-id.dto.ts
@@ -0,0 +1,4 @@
+export class FindByIDDto {
+  readonly wordID: string;
+  readonly selectedFields: string[];
+}
--- src/database/word-entry/word-entry.service.ts
+++ src/database/word-entry/word-entry.service.ts
@@ -1,11 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { WordEntry } from './schemas/word-entry.schema';
+import { WordEntry, WordEntryDocument } from './schemas/word-entry.schema';
 import { Model } from 'mongoose';
+import { FindByIDDto } from './dto/find-by-id.dto';
 
 @Injectable()
 export class WordEntryService {
   constructor(
     @InjectModel(WordEntry.name) private wordEntryModel: Model<WordEntry>,
   ) {}
+
+  async findByID(findByIDDto: FindByIDDto): Promise<WordEntryDocument> {
+    const { wordID, selectedFields } = findByIDDto;
+    const selectionQuery = selectedFields.reduce(
+      (accumulatedQuery, field) => accumulatedQuery + ` +${field}`,
+      '',
+    );
+    try {
+      const wordEntry = await this.wordEntryModel
+        .findById(wordID)
+        .select(selectionQuery);
+      if (!wordEntry) throw new BadRequestException('Word entry not found');
+      return wordEntry;
+    } catch (err) {
+      throw err;
+    }
+  }
 }
