75b4c7223dfefeebc7f7d60ba76d7525b35126c0
VincentBai-dotcom <butcheryyy@gmail.com>
2023-12-13 16:21:20 -0500

    mongodb setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { OpenaiModule } from './openai/openai.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [OpenaiModule, ConfigModule.forRoot()],
+  imports: [
+    OpenaiModule,
+    MongooseModule.forRoot(process.env.MONGODB_URL),
+    ConfigModule.forRoot(),
+  ],
   controllers: [],
   providers: [],
 })
