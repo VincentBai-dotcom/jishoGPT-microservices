7230c1683b23e592a83483554e321d65e485d7c7
VincentBai-dotcom <butcheryyy@gmail.com>
2023-12-18 23:55:59 -0500

    add object type decorator for graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { OpenaiModule } from './openai/openai.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { WordEntryModule } from './database/word-entry/word-entry.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
@@ -10,6 +12,9 @@ import { WordEntryModule } from './database/word-entry/word-entry.module';
     WordEntryModule,
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URL),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+    }),
   ],
   controllers: [],
   providers: [],
--- src/database/word-entry/schemas/word-entry.schema.ts
+++ src/database/word-entry/schemas/word-entry.schema.ts
@@ -1,32 +1,45 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument, Document } from 'mongoose';
+import { HydratedDocument, Document, Schema as MongooseSchema } from 'mongoose';
 import { Definition, DefinitionSchema } from './definition.schema';
+import { ObjectType, Field } from '@nestjs/graphql';
 
 export type WordEntryDocument = HydratedDocument<WordEntry>;
 
 @Schema()
+@ObjectType()
 export class WordEntry extends Document {
+  @Field(() => String)
+  _id: MongooseSchema.Types.ObjectId;
+
+  @Field(() => String)
   @Prop({ required: true })
   word: string;
 
+  @Field(() => String)
   @Prop({ required: true })
   pronunciation: string;
 
+  @Field(() => [DefinitionSchema])
   @Prop({ type: [DefinitionSchema] })
   definitions: Definition[];
 
+  @Field(() => String)
   @Prop({ select: false })
   description: string;
 
+  @Field(() => [String])
   @Prop({ type: [String], select: false })
   synonyms: string[];
 
+  @Field(() => String)
   @Prop({ select: false })
   usageContext: string;
 
+  @Field(() => Boolean)
   @Prop()
   isVerb: boolean;
 
+  @Field(() => String)
   @Prop({ select: false })
   conjugation: string;
 }
